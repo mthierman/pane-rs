c5b2f7b45d173b71b1c16a2b498185df5670e806
mthierman <mthierman@gmail.com>
2025-11-11 11:38:13 -0700

    More clear disable build

--- build.rs
+++ build.rs
@@ -1,54 +1,50 @@
 use std::{env, error::Error, path::PathBuf, process::Command};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    if true {
-        return Ok(());
-    }
+    // if env::var("TARGET")?.ends_with("windows-msvc") {
+    //     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
 
-    if env::var("TARGET")?.ends_with("windows-msvc") {
-        let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    //     let data = env::current_dir()?.join("data");
+    //     let data_manifest = data.join("app.manifest").canonicalize()?;
+    //     println!("cargo:rerun-if-changed={}", data_manifest.display());
+    //     let data_icon = data.join("app.ico").canonicalize()?;
+    //     println!("cargo:rerun-if-changed={}", data_icon.display());
+    //     let data_resource = data.join("app.rc").canonicalize()?;
+    //     println!("cargo:rerun-if-changed={}", data_resource.display());
 
-        let data = env::current_dir()?.join("data");
-        let data_manifest = data.join("app.manifest").canonicalize()?;
-        println!("cargo:rerun-if-changed={}", data_manifest.display());
-        let data_icon = data.join("app.ico").canonicalize()?;
-        println!("cargo:rerun-if-changed={}", data_icon.display());
-        let data_resource = data.join("app.rc").canonicalize()?;
-        println!("cargo:rerun-if-changed={}", data_resource.display());
+    //     if !data_manifest.exists() {
+    //         println!("cargo:warning={} not found", data_manifest.display());
+    //     } else if let Some(path) = data_manifest.to_str() {
+    //         println!("cargo:rustc-link-arg-bins=/MANIFEST:EMBED");
+    //         println!("cargo:rustc-link-arg-bins=/MANIFESTINPUT:{}", path);
+    //     } else {
+    //         println!(
+    //             "cargo:warning=Manifest file is not valid UTF-8: {:?}",
+    //             data_manifest
+    //         );
+    //     }
 
-        if !data_manifest.exists() {
-            println!("cargo:warning={} not found", data_manifest.display());
-        } else if let Some(path) = data_manifest.to_str() {
-            println!("cargo:rustc-link-arg-bins=/MANIFEST:EMBED");
-            println!("cargo:rustc-link-arg-bins=/MANIFESTINPUT:{}", path);
-        } else {
-            println!(
-                "cargo:warning=Manifest file is not valid UTF-8: {:?}",
-                data_manifest
-            );
-        }
+    //     if !data_resource.exists() {
+    //         println!("cargo:warning={} not found", data_resource.display());
+    //     } else if let Some(stem) = data_resource.file_stem() {
+    //         let out_file = out_dir.join(stem).with_extension("res");
 
-        if !data_resource.exists() {
-            println!("cargo:warning={} not found", data_resource.display());
-        } else if let Some(stem) = data_resource.file_stem() {
-            let out_file = out_dir.join(stem).with_extension("res");
+    //         Command::new("rc")
+    //             .args([
+    //                 "/fo",
+    //                 out_file.to_str().unwrap(),
+    //                 data_resource.to_str().unwrap(),
+    //             ])
+    //             .status()?;
 
-            Command::new("rc")
-                .args([
-                    "/fo",
-                    out_file.to_str().unwrap(),
-                    data_resource.to_str().unwrap(),
-                ])
-                .status()?;
-
-            println!("cargo::rustc-link-arg-bins={}", out_file.to_str().unwrap());
-        } else {
-            println!(
-                "cargo:warning=Resource file is not valid UTF-8: {:?}",
-                data_resource
-            );
-        }
-    }
+    //         println!("cargo::rustc-link-arg-bins={}", out_file.to_str().unwrap());
+    //     } else {
+    //         println!(
+    //             "cargo:warning=Resource file is not valid UTF-8: {:?}",
+    //             data_resource
+    //         );
+    //     }
+    // }
 
     Ok(())
 }
