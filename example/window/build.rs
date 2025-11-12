6d47fe440ad2c1b39aad3937097718c3b0544b62
mthierman <mthierman@gmail.com>
2025-11-11 17:26:22 -0700

    Cleanup build

--- example/window/build.rs
+++ example/window/build.rs
@@ -1,10 +1,14 @@
 use pane::*;
 use std::{env, path::PathBuf};
+use windows::{
+    Win32::Foundation::ERROR_FILE_NOT_FOUND,
+    core::{Error, Result},
+};
 
-fn main() {
-    println!("cargo::rustc-link-arg-bins=/WX");
-
+fn main() -> Result<()> {
     if env::var("TARGET").unwrap().ends_with("windows-msvc") {
+        println!("cargo::rustc-link-arg-bins=/WX");
+
         let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
             .parent()
             .unwrap()
@@ -16,14 +20,20 @@ fn main() {
 
         if !data.exists() {
             println!("cargo:warning={} not found", data.display());
+            return Err(Error::new(
+                ERROR_FILE_NOT_FOUND.to_hresult(),
+                "File not found",
+            ));
         }
 
-        embed_manifest(&data.join("app.manifest")).unwrap();
+        embed_manifest(&data.join("app.manifest"))?;
+
         compile_resource(
             "x64",
             &data.join("app.rc"),
             &PathBuf::from(env::var("OUT_DIR").unwrap()),
-        )
-        .unwrap();
+        )?;
     }
+
+    Ok(())
 }
